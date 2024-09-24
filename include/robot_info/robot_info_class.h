aca2f05874220565b93c7638886adaf479c436d9
Kailash Khadka <k.khadka343@gmail.com>
2024-09-24 00:47:25 +0000

    Part1-Step1

---
+++ include/robot_info/robot_info_class.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ros/node_handle.h"
+#include <ros/ros.h>
+using namespace std;
+
+class RobotInfo {
+protected:
+  string robot_description; // the brand or make
+  string serial_number;     // robot's serial number
+  string ip_address;        // robot's IP Address
+  string firmware_version;  // robot's Firmware version
+
+  ros::Publisher info_publisher; // publish all info
+  ros::NodeHandle nh_;
+
+public:
+  RobotInfo(string description, string serial, string ip, string firmware);
+  virtual void publish_data();
+};
---
+++ src/agv_robot_info_class.cpp
@@ -0,0 +1,31 @@
+#include "../include/robot_info/agv_robot_info_class.h"
+#include <iomanip> // Include for std::setprecision
+#include <robotinfo_msgs/RobotInfo10Fields.h>
+#include <sstream> // Include for std::ostringstream
+#include <string>
+
+using namespace std;
+
+AGVRobotInfo::AGVRobotInfo(string description, string serial, string ip_add,
+                           string firmware, double max_payload)
+    : RobotInfo(description, serial, ip_add, firmware),
+      maximum_payload(max_payload) {
+  ROS_INFO("AGV Robot Node created");
+}
+
+void AGVRobotInfo::publish_data() {
+  robotinfo_msgs::RobotInfo10Fields message;
+  message.data_field_01 = "robot_description: " + this->robot_description;
+  message.data_field_02 = "serial_number: " + this->serial_number;
+  message.data_field_03 = "ip_address: " + this->ip_address;
+  message.data_field_04 = "firmware_version: " + this->firmware_version;
+
+  // Create a string stream to format maximum_payload
+  std::ostringstream oss;
+  oss << std::fixed << std::setprecision(0)
+      << this->maximum_payload; // Format as integer
+  message.data_field_05 = "maximum_payload: " + oss.str() + " kg";
+
+  this->info_publisher.publish(message);
+  ROS_INFO("AGV Robot Info Published.");
+}
