4bec8e28a181c0d6cc3b9019a52ed46d487444f8
Kailash Khadka <k.khadka343@gmail.com>
2024-09-24 18:31:21 +0000

    Part1-Step2

--- src/agv_robot_info_class.cpp
+++ src/agv_robot_info_class.cpp
@@ -1,3 +1,4 @@
+#include "../include/robot_info/hydraulic_system_monitor.h"
 #include "../include/robot_info/robot_info_class.h"
 #include <robotinfo_msgs/RobotInfo10Fields.h>
 #include <string>
@@ -7,16 +8,19 @@ using namespace std;
 class AGVRobotInfo : public RobotInfo {
 private:
   double maximum_payload; // max weight robot can transport, only AGVs
+  HydraulicSystemMonitor hydraulic_system_monitor;
+
 public:
   AGVRobotInfo(string description, string serial, string ip, string firmware,
-               double max_payload);
+               double max_payload, HydraulicSystemMonitor *sys_monitor);
   void publish_data() override;
 };
 
 AGVRobotInfo::AGVRobotInfo(string description, string serial, string ip_add,
-                           string firmware, double max_payload)
+                           string firmware, double max_payload,
+                           HydraulicSystemMonitor *sys_monitor)
     : RobotInfo(description, serial, ip_add, firmware),
-      maximum_payload(max_payload) {
+      maximum_payload(max_payload), hydraulic_system_monitor(*sys_monitor) {
   ROS_INFO("AGV Robot Node created");
 }
 
@@ -28,7 +32,15 @@ void AGVRobotInfo::publish_data() {
   message.data_field_04 = "firmware_version: " + this->firmware_version;
   message.data_field_05 =
       "maximum_payload: " + to_string(this->maximum_payload);
-
+  message.data_field_06 =
+      "hydraulic_oil_temperature: " +
+      this->hydraulic_system_monitor.get_hydraulic_oil_temperature() + "C";
+  message.data_field_07 =
+      "hydraulic_oil_tank_fill_level: " +
+      this->hydraulic_system_monitor.get_hydraulic_oil_tank_fill_level() + "%";
+  message.data_field_08 =
+      "hydraulic_oil_pressure: " +
+      this->hydraulic_system_monitor.get_hydraulic_oil_pressure() + " bar";
   this->info_publisher.publish(message);
   ROS_INFO("AGV Robot Info Published.");
 }
